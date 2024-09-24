d9900b64cf7fd1f8bb16b3117a8a8f8f8588ed9d
Sergio Ortiz <yhersua08@gmail.com>
2024-09-24 12:35:10 -0500

    se agregan los scripts de proveedores en proyecto minitienda

--- scrip_minitienda.sql
+++ scrip_minitienda.sql
@@ -152,3 +152,39 @@ BEGIN
     WHERE usu_id = v_id;
 END//
 DELIMITER ;
+-- Proveedores*****************
+DELIMITER // 
+CREATE PROCEDURE procInsertProveedores(IN v_nit VARCHAR(45), IN v_nombre VARCHAR(100))
+BEGIN 
+    INSERT INTO tbl_proveedores (prov_nit, prov_nombre) VALUES (v_nit, v_nombre);
+END//
+DELIMITER ;
+-- Actualizar
+DELIMITER // 
+CREATE PROCEDURE procUpdateProveedores(
+    IN v_id INT, 
+    IN v_nit VARCHAR(45), 
+    IN v_nombre VARCHAR(100)
+)
+BEGIN
+    UPDATE tbl_proveedores
+    SET prov_nit = v_nit, prov_nombre = v_nombre
+    WHERE prov_id = v_id;
+END//
+DELIMITER ;
+-- Mostrar
+DELIMITER //
+CREATE PROCEDURE procSelectProveedores()
+BEGIN
+    SELECT prov_id, prov_nit, prov_nombre 
+    FROM tbl_proveedores;
+END//
+DELIMITER ;
+-- Eliminar
+DELIMITER //
+CREATE PROCEDURE procDeleteProveedor(IN v_id INT)
+BEGIN
+    DELETE FROM tbl_proveedores 
+    WHERE prov_id = v_id;
+END//
+DELIMITER ;
